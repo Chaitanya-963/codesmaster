0ff59f6b3ee14e0867790903f029008fb1325ec4
Chaitanya-963 <chaitanyamehetre0301@gmail.com>
2025-06-07 11:54:22 +0530

    create checkAdmin middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,8 @@ import cookieParser from "cookie-parser";
 
 
 
-
 import authRoutes from "./routes/auth.routes.js";
+import problemRoutes from "./routes/problem.routes.js";
 
 
 
@@ -22,6 +22,7 @@ app.get("/", (req,res)=>{
 })
 
 app.use("/api/v1/auth" , authRoutes)
+app.use("/api/v1/problems", problemRoutes)
 
 
 app.listen(process.env.PORT, ()=>{
